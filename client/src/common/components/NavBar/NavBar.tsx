51aa72663bcbf096817dabe7ef8ff1e82d293e3d
= <=>
2021-07-20 09:58:53 +0100

    implemented nav bar with sub menu

--- client/src/common/components/NavBar/NavBar.tsx
+++ client/src/common/components/NavBar/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Menu } from "antd";
 import { MenuMode } from "antd/lib/menu";
+const { SubMenu } = Menu;
 
 export type NavItem = {
   url: string;
@@ -18,18 +19,24 @@ const NavBar = ({ navItems, mode, onClick }: NavBarProps): JSX.Element => {
   // return <nav className="border-b h-12 border-grey-50">Navbar</nav>;
 
   return (
-    <>
-      <Menu mode={mode || "horizontal"}>
-        {navItems.map((item: NavItem) => {
-          return (
-            <Menu.Item key={item.label} onClick={() => onClick(item.url)}>
-              {item.label}
-              {/* if item.items show submenu */}
-            </Menu.Item>
-          );
-        })}
-      </Menu>
-    </>
+    <Menu mode={mode || "horizontal"}>
+      {navItems.map((item: NavItem) => {
+        return item.items ? (
+          <SubMenu key="SubMenu" title={item.label}>
+            {item.items.map((subItem, index) => {
+              console.log({ subItem });
+              return (
+                <Menu.Item key={`setting: ${subItem.label}`}>{subItem.label}</Menu.Item>
+              );
+            })}
+          </SubMenu>
+        ) : (
+          <Menu.Item key={item.label} onClick={() => onClick(item.url)}>
+            {item.label}
+          </Menu.Item>
+        );
+      })}
+    </Menu>
   );
 };
 
